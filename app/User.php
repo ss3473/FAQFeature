7052f4e624f3a08cea01d725a880ad314afcea24
sagar <ss3473@njit.edu>
2019-08-06 19:47:16 -0400

    Task: Added answer migrate, model, and made answer seeder and added tests for user profile (cleanup), and answer tests.

--- app/User.php
+++ app/User.php
@@ -72,5 +72,10 @@ class User extends Authenticatable
         return $this->hasMany('App\Question');
     }
 
+    public function answers()
+    {
+        return $this->hasMany('App\Answer');
+    }
+
 }
 
