f11673287ba2895f4ec4f33b375e1efc42ff07a9
sagar <ss3473@njit.edu>
2019-08-08 01:36:54 -0400

    Added Heroku Database configuration.

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$heroku_db_url = parse_url(env('DATABASE_URL', "postgres://forge:forge@localhost:5432/forge"));
 use Illuminate\Support\Str;
 
 return [
@@ -76,6 +76,16 @@ return [
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
+        ],
+        'pg-heroku' => [
+            'driver' => 'pgsql',
+            'host' => $heroku_db_url['host'],
+            'database' => substr($heroku_db_url['path'], 1),
+            'username' => $heroku_db_url['user'],
+            'password' => $heroku_db_url['pass'],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
         ],
 
         'sqlsrv' => [
